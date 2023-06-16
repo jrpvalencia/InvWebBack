2009ef03a78ce94a9bf93672f0baef5e6793d357
jose robinson puliche valencia <jrpvalencia612@gmail.com>
2023-06-16 17:11:33 -0500

    migraciones

---
+++ database/migrations/2023_06_16_212357_create_carrito_de_compras_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carrito_de_compra', function (Blueprint $table) {
+
+            
+            $table->id();
+            $table->integer('idCompraAgrega');
+            $table->integer('cantidadProducto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carrito_de_compra');
+    }
+};
---
+++ database/migrations/2023_06_16_220415_create_devoluciones_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devolucions', function (Blueprint $table) {
+            $table->id();
+            $table->text("motivo");
+            $table->integer("idPedido");
+            $table->integer("idPqr");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devoluciones');
+    }
+};
